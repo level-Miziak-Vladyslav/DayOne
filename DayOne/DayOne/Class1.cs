04f0502035e2182f8edc2f5cfa52aefbb29ba421
level-Miziak-Vladyslav <parado9921@gmail.com>
2018-11-04 22:20:00 +0100

    Final 041118

--- DayOne/DayOne/Class1.cs
+++ DayOne/DayOne/Class1.cs
@@ -9,60 +9,100 @@ namespace DayOne
 
     class Creation: ICustomCollection
     {
-
-
+        ICustomCollectionItem lastIndex;
+        ICustomCollectionItem StartElement;
+        public Creation()
+        {
+            lastIndex = new TheElement();
+            lastIndex.Index = 0;
+            lastIndex.Next = null;
+            lastIndex.Value = "start object # " + (lastIndex.Index).ToString();
+            StartElement = lastIndex;
+            Console.WriteLine(StartElement.Value.ToString() + "  index = " + StartElement.Index.ToString()+ " was added");
+        }
         void ICustomCollection.Add(ICustomCollectionItem item)
         {
-            throw new NotImplementedException();
+            item.Index = lastIndex.Index+1;
+            lastIndex.Next = item;
+            lastIndex = item;
+            Console.WriteLine(lastIndex.Value.ToString() + "  index = " + lastIndex.Index.ToString() + " was added");
+
         }
 
         ICustomCollectionItem ICustomCollection.Crop(int index)
         {
-            throw new NotImplementedException();
+            ICustomCollectionItem z = StartElement;
+            ICustomCollectionItem x = StartElement;
+            if (index >= lastIndex.Index)
+            {
+                for (int i = 0; i < lastIndex.Index; i++)
+                {
+                    x = z;
+                    z = x.Next;
+                }
+                lastIndex = x;
+                return z;
+            }
+            if (index <= 0)
+            {
+                z = StartElement;
+                StartElement = StartElement.Next;
+                x = z;
+            }
+            else
+            {
+                while (z.Index != index)
+                {
+                    x = z;
+                    z = z.Next;
+                }
+                x.Next = z.Next;
+            }
+            for (int j = x.Next.Index; j < (lastIndex.Index + 1); j++)
+                {
+                    x.Next.Index = x.Next.Index - 1;
+                    x = x.Next;
+                }
+                lastIndex = x;
+            
+            Console.WriteLine(z.Value.ToString() + "  index = " + z.Index.ToString() + " was cropped");
+            return z;
         }
 
         ICustomCollectionItem ICustomCollection.GetAt(int index)
         {
-            throw new NotImplementedException();
+            if (index > lastIndex.Index) return null;
+            ICustomCollectionItem z = StartElement;
+            int i = lastIndex.Index;
+            while (z.Index != index)
+            {
+                z = z.Next;
+            }
+            return z;
         }
-
         void ICustomCollection.PrintAllValues()
         {
-            throw new NotImplementedException();
+            ICustomCollectionItem z = StartElement;
+            int i = lastIndex.Index;
+            while (i != -1)
+            {
+                Console.WriteLine(z.Value.ToString()+ "  index = " + z.Index.ToString());
+                z = z.Next;
+                i--;
+            }
         }
     }
     class TheElement : ICustomCollectionItem
     {
-        ICustomCollectionItem n;
-        object v;
-        int ind;
-        public ICustomCollectionItem Next
-        {
-            get { return n; }
-            set { n = value; }
-        }
-        public object Value
-        {
-            get { return v; }
-            set { v = value; }
-        }
-        public int  Index
-        {
-            get { return ind; }
-            set { ind = value; }
-        }
+        public ICustomCollectionItem Next { get; set; }
+        public object Value { get; set; }
+        public int Index { get; set; }
         public TheElement()
         {
             Index = 0;
             Next = null;
             Value = "object # " + (Index + 1).ToString();
-        }
-        public TheElement(ICustomCollectionItem it)
-        {
-            Index = it.Index +1;
-            it.Next = this;
-            Next = null;
-            Value = "object # " + (Index + 1).ToString();
+
         }
     }
 }
--- DayOne/DayOne/Program.cs
+++ DayOne/DayOne/Program.cs
@@ -10,25 +10,73 @@ namespace DayOne
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine();
-            Console.WriteLine("enter the number of basic Elements");
-            int a = int.Parse(Console.ReadLine());
-            ICustomCollectionItem ie;
-            ie = new TheElement();
+            int keypress;
+            try
+            {
+                Console.WriteLine("enter the number of basic Elements");
+                int a = int.Parse(Console.ReadLine());
+                ICustomCollection s = new Creation();
                 for (int i = 0; i < a - 1; i++)
                 {
-                    Console.WriteLine(ie.Value.ToString());
-                    ie = new TheElement(ie);
-                    Console.WriteLine(ie.Value.ToString());
-                }           
-            ICustomCollection s = new Creation();
-            Console.WriteLine("enter the Name of new Element");
-            string b = Console.ReadLine();
-            ICustomCollectionItem add = new TheElement();
-            s.Add(add);
+                    ICustomCollectionItem ie = new TheElement();
+                    ie.Value = "object # " + (i + 1).ToString();
+                    s.Add(ie);
+                }
+                Console.WriteLine();
 
-            Console.WriteLine();
+                do
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Enter task number");
+                    Console.WriteLine("1 for enter the Name of new Element");
+                    Console.WriteLine("2 for crop the Element");
+                    Console.WriteLine("3 for get Element");
+                    Console.WriteLine("4 for print all elements");
+                    Console.WriteLine();
+
+                    keypress = int.Parse(Console.ReadLine()); // read keystrokes
+
+                    if (keypress == 1)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("enter the Name of new Element");
+                        string b = Console.ReadLine();
+                        ICustomCollectionItem add = new TheElement();
+                        add.Value = b;
+                        s.Add(add);
+                        Console.WriteLine();
+                    }
+                    if (keypress == 2)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("enter index of Element for crop");
+                        int c = int.Parse(Console.ReadLine());
+                        Console.WriteLine(s.Crop(c).Value.ToString() + " was cropped");
+                        Console.WriteLine();
+                    }
+                    if (keypress == 3)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("enter index of Element for get");
+                        int d = int.Parse(Console.ReadLine());
+                        Console.WriteLine(s.GetAt(d).Value.ToString() + "  index = " + s.GetAt(d).Index.ToString() + " was getted");
+                        Console.WriteLine();
+                    }
+                    if (keypress == 4)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(" print all elements ");
+                        s.PrintAllValues();
+                        Console.WriteLine();
+                    }
+                } while (keypress != 0);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ошибка поймана" + exc);
+                Console.WriteLine();
+            }
         }
     }
 }
